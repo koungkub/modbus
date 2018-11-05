6a6507ffc0028ff0dd29eaa5920bde0229d5767d
koung <59070025@it.kmitl.ac.th>
2018-11-05 19:14:43 +0700

    fix bug

--- config/config.js
+++ config/config.js
@@ -20,10 +20,18 @@ module.exports = {
     storage: ':memory:'
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+    autoreconnect: true,
+    pool: {
+      max: 30,
+      min: 1,
+      acquire: 30000,
+      idle: 10000
+    }
   }
 }
