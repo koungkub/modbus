608bb05c7193113981900c7455c7e3f64451a7f4
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2019-01-04 23:19:29 +0700

    Clean mock data

--- web/src/components/Factory/FactoryPage.jsx
+++ web/src/components/Factory/FactoryPage.jsx
@@ -9,42 +9,7 @@ const styles = () => ({
   }
 });
 
-const factoryMock = {
-  id: 1,
-  factory: 'ccna',
-  name: 'ชื่อโรงงาน',
-  address: 'ที่อยู่โรงงาน',
-  tel: '082-6666666',
-  rpis: [
-    {
-      id: 1,
-      mac_address: 'b8:27:eb:3e:b9:58',
-      modbus_ip: '192.168.1.1',
-      Channels: [
-        {
-          id: 1,
-          channel: 0,
-          in_max: 1,
-          in_min: 2,
-          out_max: 3,
-          out_min: 4,
-          status: true,
-        },
-        {
-          id: 2,
-          channel: 1,
-          in_max: 1,
-          in_min: 2,
-          out_max: 3,
-          out_min: 4,
-          status: false,
-        },
-      ],
-    },
-  ],
-};
-
-const FactoryPage = ({ factory = factoryMock, classes }) => {
+const FactoryPage = ({ factory, classes }) => {
   console.log(factory)
   return (
     <div>
