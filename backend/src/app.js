d3df184eb10c287fa192eace960f3bedd46eec00
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2019-01-05 23:21:52 +0700

    Add allow methods

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,8 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header("Access-Control-Allow-Methods", 'GET,HEAD,OPTIONS,POST,PUT')
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
   next()
 })
 
