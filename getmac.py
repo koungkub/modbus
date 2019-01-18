345d3bf5611362c1774323ed6bb03e0337e91c71
koungkub <1997jirasak@gmail.com>
2019-01-18 15:47:17 +0700

    add getmac.py

---
+++ getmac.py
@@ -0,0 +1,8 @@
+def getMac(interface='eth0'):
+    try:
+        mac_address = open('/sys/class/net/%s/address' %interface).read()
+    except:
+        logger.error("Can't get mac-address from Raspberry Pi")
+        mac_address = "00:00:00:00:00:00"
+    finally:
+        return mac_address[0:17]
